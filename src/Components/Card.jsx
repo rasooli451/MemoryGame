012990929c9f1e6c881fd6fb4562a7102cdf07ca
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-04 23:28:57 -0400

    implemented the function for handling user input which will then be passed up to App.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,10 +2,12 @@
 import { useState } from "react";
 
 export default function Card({card, clicked}){
-    let disected = card == 0 ? 0 : card.split(",");
-    return <div className="card">
-        <img src={disected[0]} alt={disected[1]}/>
-        <p className="name">{disected[1]}</p>
+    function handleInput(){
+        clicked(card.clicked , card.key);
+    }
+    return <div className="card" onClick={handleInput}>
+        <img src={card.sprite} alt={card.name}/>
+        <p className="name">{card.name}</p>
     </div>
 
 }
